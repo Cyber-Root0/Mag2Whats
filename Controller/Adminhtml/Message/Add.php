c9d24f57a3a6e95010e25e2263fe75cb201f89ce
Bruno Alves <boteistem@gmail.com>
2025-02-04 20:59:05 +0000

    feix(listing): fix bug on listing grid page

---
+++ Controller/Adminhtml/Message/Add.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) 2025 Bruno Alves. All rights reserved.
+ *
+ * This file is part of the CRT0 Mag2Whats module for Magento 2.
+ *
+ * @package   CRT0_Mag2Whats
+ * @author    Bruno Alves <boteistem@gmail.com>
+ * @license   MIT
+ * @link      https://github.com/Cyber-Root0
+ * @version   1.0.0
+ * @since     2025
+ */
+namespace CRT0\Mag2Whats\Controller\Adminhtml\Message;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Backend\App\Action\Context;
+use Magento\Backend\App\Action;
+class Add extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Construtor
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Custom Message'));
+        return $resultPage;
+    }
+}
